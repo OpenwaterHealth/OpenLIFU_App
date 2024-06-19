a8c3c0041da6b665ee3e2d27a6eaf864a1221266
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-06-19 12:27:34 -0400

    Add OpenLIFU application sources
    This commit adds source files generated using
    KitwareMedical/SlicerCustomAppTemplate@b27da5138ef5d79ec00e0e698ac3de85ab2adb87
    and referencing Slicer/Slicer@84b4b105088288ccfb295a7656de3be0bf1efd12
    
    Features and improvements specific to the custom application will be integrated
    in follow-up commits.
    
    Issue #1

---
+++ Applications/OpenLIFUApp/slicer-application-properties.cmake
@@ -0,0 +1,35 @@
+
+set(APPLICATION_NAME
+  OpenLIFU
+  )
+
+set(VERSION_MAJOR
+  0
+  )
+set(VERSION_MINOR
+  1
+  )
+set(VERSION_PATCH
+  0
+  )
+
+set(DESCRIPTION_SUMMARY
+  "An open-source software platform for Low Intensity Focused Ultrasound (LIFU) research and development"
+  )
+set(DESCRIPTION_FILE
+  ${Slicer_SOURCE_DIR}/README.md
+  )
+
+set(LAUNCHER_SPLASHSCREEN_FILE
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Images/SplashScreen.png"
+  )
+set(APPLE_ICON_FILE
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Icons/DesktopIcon.icns"
+  )
+set(WIN_ICON_FILE
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Icons/DesktopIcon.ico"
+  )
+
+set(LICENSE_FILE
+  "${OpenLIFU_SOURCE_DIR}/LICENSE"
+  )
