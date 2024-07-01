21ede6c8ad06a4cdf6f6afacda23a188ea78f515
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-07-01 15:53:59 -0400

    Bundle openlifu python package into app
    Issue #2

---
+++ SuperBuild/External_python-requirements.cmake
@@ -0,0 +1,22 @@
+set(proj python-requirements)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES python python-pip)
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+ExternalProject_Add(${proj}
+  ${${proj}_EP_ARGS}
+  DOWNLOAD_COMMAND ""
+  SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+  BUILD_IN_SOURCE 1
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install -r ${OpenLIFU_SOURCE_DIR}/SuperBuild/requirements.txt
+  LOG_INSTALL 1
+  DEPENDS
+    ${${proj}_DEPENDENCIES}
+  )
+
+ExternalProject_GenerateProjectDescription_Step(${proj} VERSION ${_version})
